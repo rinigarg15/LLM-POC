1ed23aa9ff686b3f2a4a61838dc8227fc103f681
rinigarg15 <rinigarg20@gmail.com>
2024-05-14 12:35:36 +0530

    Update net_app_routes.py

--- Routes/net_app_routes.py
+++ Routes/net_app_routes.py
@@ -5,20 +5,13 @@ import os
 
 
 router = APIRouter()
-embedded_tables_unstructured_pack = None
+    
 
-
-@router.on_event("startup")
-def startup_event():
-    global embedded_tables_unstructured_pack
-
+@router.get("/response")
+def response(query: str):
     embedded_tables_unstructured_pack = EmbeddedTablesUnstructuredRetrieverPack(
         os.getenv("HTML_PATH"),
         nodes_save_path=os.getenv("PKL_PATH")
     )
-
-@router.get("/response")
-def response(query: str):
-
     response = embedded_tables_unstructured_pack.run(query)
     return response.response
